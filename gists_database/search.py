f11b35721214abafcf5fcdf3363c0d8fd217e833
SagarM11 <sagarm11@gmail.com>
2020-06-07 23:08:35 +0530

    Update search.py

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,15 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    
+    if github_id is None and created_at is None:
+        query = 'select * from gists'
+    elif github_id is not None and created_at is None:
+        query = 'select * from gists where github_id = {github_id}'.format(github_id = github_id)
+    elif github_id is not None and created_at is not None:
+        query = 'select * from gists where github_id = {github_id} and created_at = {created_at}'.format(github_id = github_id,created_at = created_at)
+    else:
+        query = 'select * from gists where created_at = {created_at}'.format(created_at = created_at)
+        
+     cursor = db_connect.execute(query)
+     return(cursor.fetchall())
